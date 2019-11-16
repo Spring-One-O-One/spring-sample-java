8a7421a369172fd4fe9876ca79cfb6ed0d67d0df
William Mallett <mallett002@gmail.com>
2019-11-15 19:16:40 -0600

    property injection

--- src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/HibernateCustomerRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.pluralsight.repository;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -11,8 +12,17 @@ import com.pluralsight.model.Customer;
 @Repository("customerRepository")
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
 
+    @Value("${dbUsername}")
+    private String dbUsername;
+
+    @Value("${dbPassword}")
+    private String dbPassword;
+
     @Override
     public List<Customer> findAll() {
+        System.out.println("dbUsername: " + dbUsername);
+        System.out.println("dbPassword: " + dbPassword);
+
         List<Customer> customers = new ArrayList<>();
 
         Customer customer = new Customer();
